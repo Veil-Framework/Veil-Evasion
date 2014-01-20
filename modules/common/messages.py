cabc2a33403d276d1a8478fe26457b6c573655bb
HarmJ0y <will@harmj0y.net>
2014-01-19 20:05:35 -0500

    LHOST extraction bug fix for handler resource file generation.

--- modules/common/messages.py
+++ modules/common/messages.py
@@ -13,7 +13,7 @@ def title():
     """
     os.system(settings.TERMINAL_CLEAR)
     print '========================================================================='
-    print ' Veil-Evasion | [Version]: 2.4.2'
+    print ' Veil-Evasion | [Version]: 2.4.3'
     print '========================================================================='
     print ' [Web]: https://www.veil-framework.com/ | [Twitter]: @VeilFramework'
     print '========================================================================='
