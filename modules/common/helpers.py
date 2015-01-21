e5f78c068630d7bc90da3cc2bbd4ab3f12c38482
Chris Truncer <chris@christophertruncer.com>
2015-01-20 20:29:41 -0500

    Jan V-Day and meterpreter update

--- modules/common/helpers.py
+++ modules/common/helpers.py
@@ -3,13 +3,13 @@ Contains any miscellaneous helper methods useful across multiple modules.
 
 """
 
-import random, string, base64, zlib, re, textwrap, commands, settings, os
+import random, string, base64, zlib, re, textwrap, commands, settings, os, sys
 
-    
+
 def color(string, status=True, warning=False, bold=True):
     """
     Change text color for the linux terminal, defaults to green.
-    
+
     Set "warning=True" for red.
     """
     attr = []
@@ -22,14 +22,16 @@ def color(string, status=True, warning=False, bold=True):
     if bold:
         attr.append('1')
     return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
-    
+
+
 def inflate( b64string ):
     """
     Decode/decompress a base64 string. Used in powershell invokers.
     """
     decoded_data = base64.b64decode( b64string )
     return zlib.decompress( decoded_data , -15)
-    
+
+
 def deflate( string_val ):
     """
     Compress/base64 encode a string. Used in powershell invokers.
@@ -38,11 +40,13 @@ def deflate( string_val ):
     compressed_string = zlibbed_str[2:-4]
     return base64.b64encode( compressed_string )
 
+
 def LHOST():
     """
     Return the IP of eth0
-    """ 
+    """
     return commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
+
 
 def isValidHostname(hostname):
     """
@@ -53,7 +57,8 @@ def isValidHostname(hostname):
     allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
     return all(allowed.match(x) for x in hostname.split("."))
 
-def formatLong(title,message, frontTab=True, spacing=16):
+
+def formatLong(title, message, frontTab=True, spacing=16):
     """
     Print a long title:message with our standardized formatting.
     Wraps multiple lines into a nice paragraph format.
@@ -92,11 +97,13 @@ def randomString(length=-1):
     random_string = ''.join(random.choice(string.ascii_letters) for x in range(length))
     return random_string
 
+
 def randomKey(b=32):
     """
     Returns a random string/key of "b" characters in length, defaults to 32
     """
     return ''.join(random.choice(string.ascii_letters + string.digits + "{}!@#$^&()*&[]|,./?") for x in range(b))
+
 
 def randomNumbers(b=6):
     """
@@ -116,11 +123,13 @@ def randomLetter():
     """
     return random.choice(string.ascii_letters)
 
+
 def shuffle(l):
     """
     Shuffle the passed list.
     """
     random.shuffle(l)
+
 
 def obfuscateNum(N, mod):
     """
@@ -136,8 +145,8 @@ def obfuscateNum(N, mod):
 
 
 def selfcontained_patch():
-    if os.path.exists(settings.METASPLOIT_PATH + "/vendor/bundle/ruby/1.9.1/gems/meterpreter_bins-0.0.12/meterpreter/metsrv.x86.dll"):
-        metsrvPath = settings.METASPLOIT_PATH + "/vendor/bundle/ruby/1.9.1/gems/meterpreter_bins-0.0.12/meterpreter/metsrv.x86.dll"
+    if os.path.exists(settings.METASPLOIT_PATH + "/vendor/bundle/ruby/1.9.1/gems/meterpreter_bins-0.0.13/meterpreter/metsrv.x86.dll"):
+        metsrvPath = settings.METASPLOIT_PATH + "/vendor/bundle/ruby/1.9.1/gems/meterpreter_bins-0.0.13/meterpreter/metsrv.x86.dll"
     else:
         print "[*] Error: You either do not have the latest version of Metasploit or"
         print "[*] Error: do not have your METASPLOIT_PATH set correctly in your settings file."
--- modules/common/messages.py
+++ modules/common/messages.py
@@ -8,7 +8,7 @@ import settings
 import helpers
 
 
-version = "2.15.3"
+version = "2.16.0"
 
 
 # try to find and import the settings.py config file
