1837cddecf40a4acb965c628af93aa1b8afad7d3
Chris Truncer <chris@christophertruncer.com>
2015-02-02 14:10:50 -0500

    Fixed pyinstaller pyherion bug

--- modules/payloads/auxiliary/pyinstaller_wrapper.py
+++ modules/payloads/auxiliary/pyinstaller_wrapper.py
@@ -8,17 +8,19 @@ by @harmj0y
 """
 
 from modules.common import helpers
+from modules.common import encryption
 import settings
 
+
 class Payload:
-    
+
     def __init__(self):
         # required options
         self.description = "Auxiliary pyinstaller wrapper for python source files"
         self.language = "python"
         self.rating = "Normal"
         self.extension = "py"
-        
+
         self.required_options = {   "python_source"  :  ["", "Python source file to compile with pyinstaller"],
                                     "compile_to_exe" :  ["Y", "Compile to an executable"],
                                     "use_pyherion"   :  ["N", "Use the pyherion encrypter"] }
@@ -27,7 +29,7 @@ class Payload:
     def generate(self):
 
         python_source = self.required_options["python_source"][0]
-        
+
         try:
             # read in the python source
             f = open(python_source, 'r')
@@ -36,8 +38,7 @@ class Payload:
         except IOError:
             print helpers.color("\n [!] python_source file \""+python_source+"\" not found\n", warning=True)
             return ""
-        
-        
+
         # example of how to check the internal options
         if self.required_options["use_pyherion"][0].lower() == "y":
             PayloadCode = encryption.pyherion(PayloadCode)
