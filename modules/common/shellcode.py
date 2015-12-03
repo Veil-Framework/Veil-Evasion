1a184ce763bc49152d372d45160b663e5413260c
Chris Truncer <chris@christophertruncer.com>
2015-12-03 11:46:34 -0700

    removed debug print and extra line

--- modules/common/shellcode.py
+++ modules/common/shellcode.py
@@ -464,8 +464,6 @@ class Shellcode:
                 if len(extraValues) != 0 :
                     self.msfvenomCommand += " " +  " ".join(extraValues)
                 self.msfvenomCommand += " -f c | tr -d \'\"\' | tr -d \'\\n\'"
-                print self.msfvenomCommand
-
 
     def generate(self, required_options=None):
         """
@@ -525,3 +523,5 @@ class Shellcode:
                 except:
                     return FuncShellcode[22:-1].strip()
 
+final_opt = xtra_opt.split('=')[0] + " " + xtra_opt.split('=')[1]
+                                    extraValues.append(final_opt)
